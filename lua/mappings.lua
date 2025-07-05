558f6f6f10b76e3a149b487f78c4462151349551
Proto <Humayoon04syed@gmail.com>
2025-07-05 21:53:25 +0500

    kubectl keymap

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -8,4 +8,15 @@ map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+vim.keymap.set(
+  "n",
+  "<leader>k",
+  function()
+    require("kubectl").toggle({ tab = false })
+  end,
+  { desc = "Toggle Kubectl", noremap = true, silent = true }
+)
+
+-- 👇 Quit key inside kubectl.nvim view
+vim.keymap.set("n", "q", "<Plug>(kubectl.quit)", { desc = "Quit Kubectl view", noremap = false, silent = true })
 
