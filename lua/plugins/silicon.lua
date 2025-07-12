a4db2f83ad226ed25d6b915ac31bc3433516c0a7
abdullahApt123 <abdullahusmani606@gmail.com>
2025-07-12 10:45:47 +0500

    default silicon

--- lua/plugins/silicon.lua
+++ lua/plugins/silicon.lua
@@ -1,19 +1,30 @@
-return
-{
+return {
   "narutoxy/silicon.lua",
   dependencies = { "nvim-lua/plenary.nvim" },
   config = function()
-    local silicon = require("silicon")   -- 👈 Add this line!
+    local silicon = require("silicon")
     silicon.setup({})
 
-    -- 🔧 Custom keybinds (no <Leader>)
-    vim.keymap.set("v", "<M-s>", function() silicon.visualise_api({}) end, { desc = "Silicon: selection to image" })
-    vim.keymap.set("v", "<M-S>", function() silicon.visualise_api({ to_clip = true, show_buf = true }) end,
-      { desc = "Silicon: selection + buffer to clipboard" })
+    -- 🖼 Default keymaps
+    -- Generate image of lines in a visual selection
+    vim.keymap.set("v", "<Leader>s", function()
+      silicon.visualise_api({})
+    end, { desc = "Silicon: visual selection" })
 
-    vim.keymap.set("n", "<M-s>", function() silicon.visualise_api({ to_clip = true, visible = true }) end,
-      { desc = "Silicon: visible buffer to clipboard" })
-    vim.keymap.set("n", "<M-S>", function() silicon.visualise_api({ to_clip = true }) end,
-      { desc = "Silicon: current line to clipboard" })
-  end
+    -- Generate image of a whole buffer, with lines in a visual selection highlighted
+    vim.keymap.set("v", "<Leader>bs", function()
+      silicon.visualise_api({ to_clip = true, show_buf = true })
+    end, { desc = "Silicon: buffer + selection to clipboard" })
+
+    -- Generate visible portion of a buffer
+    vim.keymap.set("n", "<Leader>s", function()
+      silicon.visualise_api({ to_clip = true, visible = true })
+    end, { desc = "Silicon: visible buffer to clipboard" })
+
+    -- Generate current buffer line in normal mode
+    vim.keymap.set("n", "<Leader>s", function()
+      silicon.visualise_api({ to_clip = true })
+    end, { desc = "Silicon: current line to clipboard" })
+  end,
 }
+
