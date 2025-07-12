aa857fdce0f1ad2fc76897e449fbeab8a3789b62
abdullahApt123 <abdullahusmani606@gmail.com>
2025-07-12 11:05:11 +0500

    silicon integrated

--- lua/plugins/silicon.lua
+++ lua/plugins/silicon.lua
@@ -1,30 +1,14 @@
-return {
-  "narutoxy/silicon.lua",
-  dependencies = { "nvim-lua/plenary.nvim" },
-  config = function()
-    local silicon = require("silicon")
-    silicon.setup({})
-
-    -- 🖼 Default keymaps
-    -- Generate image of lines in a visual selection
-    vim.keymap.set("v", "<Leader>s", function()
-      silicon.visualise_api({})
-    end, { desc = "Silicon: visual selection" })
-
-    -- Generate image of a whole buffer, with lines in a visual selection highlighted
-    vim.keymap.set("v", "<Leader>bs", function()
-      silicon.visualise_api({ to_clip = true, show_buf = true })
-    end, { desc = "Silicon: buffer + selection to clipboard" })
-
-    -- Generate visible portion of a buffer
-    vim.keymap.set("n", "<Leader>s", function()
-      silicon.visualise_api({ to_clip = true, visible = true })
-    end, { desc = "Silicon: visible buffer to clipboard" })
-
-    -- Generate current buffer line in normal mode
-    vim.keymap.set("n", "<Leader>s", function()
-      silicon.visualise_api({ to_clip = true })
-    end, { desc = "Silicon: current line to clipboard" })
-  end,
+return
+{
+	"michaelrommel/nvim-silicon",
+	lazy = true,
+	cmd = "Silicon",
+	main = "nvim-silicon",
+	opts = {
+		-- Configuration here, or leave empty to use defaults
+		line_offset = function(args)
+			return args.line1
+		end,
+	}
 }
 
