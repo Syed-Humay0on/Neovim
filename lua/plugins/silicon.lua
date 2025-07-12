9ea129663eaeb381ec9b99d7597965f8dca15362
Proto <Humayoon04syed@gmail.com>
2025-07-12 20:39:01 +0500

    Silicon config

--- lua/plugins/silicon.lua
+++ lua/plugins/silicon.lua
@@ -1,14 +1,24 @@
-return
-{
-	"michaelrommel/nvim-silicon",
-	lazy = true,
-	cmd = "Silicon",
-	main = "nvim-silicon",
-	opts = {
-		-- Configuration here, or leave empty to use defaults
-		line_offset = function(args)
-			return args.line1
-		end,
-	}
+return {
+  "michaelrommel/nvim-silicon",
+  lazy = true,
+  cmd = "Silicon",
+  main = "nvim-silicon",
+  init = function()
+    local wk = require ("which-key")
+    wk.add({
+      mode = {"v"},
+      {"<leader>s", group = "Silicon"},
+      {"<leader>sc", function() require("nvim-silicon").clip() end, desc = "copy code screenshot to clipboard"},
+      {"<leader>sf", function() require("nvim-silicon").file() end, desc = "Save code screenshot as file"},
+      {"<leader>ss", function() require("nvim-silicon").shoot() end, desc = "Create code screenshot"},
+    })
+  end,
+  opts = {
+    -- Configuration here, or leave empty to use defaults
+    theme = "Nord",
+    background = "#444a73",
+    line_offset = function(args)
+      return args.line1
+    end,
+  },
 }
-
