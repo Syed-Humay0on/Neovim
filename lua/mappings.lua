9e757b1cbdda2f98cf3a7315d7f6c46d5a458fbd
Proto <Humayoon04syed@gmail.com>
2025-07-16 06:15:14 +0500

    vi-mongo

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -8,11 +8,9 @@ map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
-vim.keymap.set(
-  "n",
-  "<leader>k",
-  function()
-    require("kubectl").toggle({ tab = true })
-  end,
-  { desc = "Toggle Kubectl", noremap = true, silent = true }
-)
+vim.keymap.set("n", "<leader>k", function()
+  require("kubectl").toggle { tab = true }
+end, { desc = "Toggle Kubectl", noremap = true, silent = true })
+-- vim.keymap.set("n", "<leader>m", function()
+--   require("mongo-nvim.pickers").document_picker()
+-- end, { desc = "Mongo: Pick Document" })
