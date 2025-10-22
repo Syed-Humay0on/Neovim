95c97fe0e1c5eebcd1c3e7558266e04e8ce36e41
Proto <Humayoon04syed@gmail.com>
2025-10-22 21:37:01 +0500

    jupynium -> nb_Classic

--- lua/plugins/jupynium.lua
+++ lua/plugins/jupynium.lua
@@ -1,21 +1,18 @@
-return
-{
+return {
   "kiyoon/jupynium.nvim",
-  build = "pip install .",
-  dependencies = { "rcarriga/nvim-notify", "stevearc/dressing.nvim" },
+  build = "pip install -e .",
+  dependencies = {
+    "rcarriga/nvim-notify",
+    "stevearc/dressing.nvim",
+  },
   config = function()
-     -- Get the correct python path dynamically
-    local python_path = vim.fn.exepath("python")  -- Works on Linux, macOS, Windows (including WSL)
-
-        -- Tell Neovim to use the same Python for all Python-based plugins
+    local python_path = vim.fn.exepath("python")
     vim.g.python3_host_prog = python_path
 
     require("jupynium").setup({
       python_host = python_path,
-      jupyter_command = "jupyter", -- assumes it's in your PATH
-      -- Optional: add this if you want logs to debug issues
-      -- log_level = "info",
+      jupyter_command = "jupyter",
+      default_notebook_URL = "localhost:8888/nbclassic", -- <- key fix
     })
   end,
 }
-
