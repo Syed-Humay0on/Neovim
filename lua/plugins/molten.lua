4f1ef1538461c40408cfd10f13234062bc6106c4
Proto <Humayoon04syed@gmail.com>
2025-10-30 20:41:54 +0500

    molten modified

--- lua/plugins/molten.lua
+++ lua/plugins/molten.lua
@@ -1,13 +1,26 @@
 return {
-    {
-        "benlubas/molten-nvim",
-        version = "^1.0.0", -- use version <2.0.0 to avoid breaking changes
-        dependencies = { "3rd/image.nvim" },
-        build = ":UpdateRemotePlugins",
-        init = function()
-            -- these are examples, not defaults. Please see the readme
-            vim.g.molten_image_provider = "image.nvim"
-            -- vim.g.molten_output_win_max_height = 20
+  {
+    "benlubas/molten-nvim",
+    version = "^1.0.0",
+    dependencies = { "3rd/image.nvim" },
+    build = ":UpdateRemotePlugins",
+    init = function()
+      vim.g.molten_image_provider = "image.nvim"
+    end,
+    config = function()
+      -- Wait until molten is available before attaching
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = "python",
+        callback = function()
+          vim.schedule(function()
+            if vim.fn.exists(":MoltenAttach") == 2 then
+              vim.cmd("MoltenAttach")
+            else
+              vim.notify("MoltenAttach not ready yet", vim.log.levels.WARN)
+            end
+          end)
         end,
-    },
+      })
+    end,
+  },
 }
