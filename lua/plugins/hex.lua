9d8e8303ebf30331909994dc5d79d48a28d9a1cc
Proto <Humayoon04syed@gmail.com>
2025-11-06 20:07:06 +0500

    hex view

--- lua/plugins/hex.lua
+++ lua/plugins/hex.lua
@@ -1,3 +1,12 @@
-return{
-   'RaafatTurki/hex.nvim'
+return {
+  "RaafatTurki/hex.nvim",
+  opts = {
+    -- optional config
+    dump_cmd = "xxd -g 1 -u", -- command used for hex dump
+    assemble_cmd = "xxd -r",  -- command used to revert hex to binary
+    filetypes = { "binary", "hex" },
+  },
+  keys = {
+    { "<leader>hx", "<cmd>HexToggle<cr>", desc = "Toggle hex view" },
+  },
 }
