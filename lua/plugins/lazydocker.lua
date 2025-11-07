e710aba81bd14d10da1fe4b30f86827789e05fcf
Proto <Humayoon04syed@gmail.com>
2025-11-08 00:26:18 +0500

    lazydocker resized to full

--- lua/plugins/lazydocker.lua
+++ lua/plugins/lazydocker.lua
@@ -23,8 +23,8 @@ return {
     require("lazydocker").setup {
       window = {
         settings = {
-          width = 0.9,
-          height = 0.9,
+          width = 1.0,
+          height = 1.0,
           border = "rounded",
           relative = "editor",
         },
