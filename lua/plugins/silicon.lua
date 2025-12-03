9f3f981065c35aa930a57ea7ab4b8174668caebe
Proto <Humayoon04syed@gmail.com>
2025-12-04 00:13:20 +0500

    silicon output destination

--- lua/plugins/silicon.lua
+++ lua/plugins/silicon.lua
@@ -4,22 +4,59 @@ return {
   cmd = "Silicon",
   main = "nvim-silicon",
   init = function()
-    local wk = require ("which-key")
-    wk.add({
-      mode = {"v"},
-      {"<leader>s", group = "Silicon"},
-      {"<leader>sc", function() require("nvim-silicon").clip() end, desc = "copy code screenshot to clipboard"},
-      {"<leader>sf", function() require("nvim-silicon").file() end, desc = "Save code screenshot as file"},
-      {"<leader>ss", function() require("nvim-silicon").shoot() end, desc = "Create code screenshot"},
-    })
+    local wk = require "which-key"
+    wk.add {
+      mode = { "v" },
+      { "<leader>s", group = "Silicon" },
+      {
+        "<leader>sc",
+        function()
+          require("nvim-silicon").clip()
+        end,
+        desc = "Copy code screenshot to clipboard",
+      },
+      {
+        "<leader>sf",
+        function()
+          require("nvim-silicon").file()
+        end,
+        desc = "Save code screenshot as file",
+      },
+      {
+        "<leader>ss",
+        function()
+          require("nvim-silicon").shoot()
+        end,
+        desc = "Create code screenshot",
+      },
+    }
   end,
   opts = {
-    -- Configuration here, or leave empty to use defaults
-    -- font = "FiraCode Nerd Font=30;Standard Symbols PS=30",
+    -- Enable this to avoid conflicts with silicon's config file
+    disable_defaults = true,
+
+    -- Your existing settings
     theme = "Nord",
     background = "#444a73",
     line_offset = function(args)
       return args.line1
+    end,
+
+    -- Custom output function that creates directory if needed
+    output = function()
+      -- Define target directory
+      local dir = "./CodeShots"
+
+      -- Create directory if it doesn't exist (works on Linux/Unix)
+      if vim.fn.isdirectory(dir) == 0 then
+        vim.fn.mkdir(dir, "p")
+      end
+
+      -- Generate unique filename with timestamp
+      local filename = os.date "%Y-%m-%d_%H-%M-%S" .. "_code.png"
+
+      -- Return full path
+      return dir .. "/" .. filename
     end,
   },
 }
